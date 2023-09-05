dbb466d3e64bf7acb410fcce297d7b87d0bceaeb
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-09-04 21:27:56 -0300

    change: CEP changed to its own class

---
+++ src/com/serratec/uteis/BuscarCEP.java
@@ -0,0 +1,82 @@
+package com.serratec.uteis;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.serratec.classes.Endereco;
+import com.serratec.dao.EnderecoDAO;
+import com.serratec.main.Main;
+
+public class BuscarCEP {
+	public static Scanner in = new Scanner(System.in);
+	//função para buscar cep
+    public static String buscarCep() 
+    {
+    	//PROCURAR CEP
+    	String logradouro;
+    	String tipo_logradouro = null;
+    	String bairro;
+    	String cidade;
+    	String uf;
+    	String endereco;
+    	String numero_residencia = "";
+    	String complemento = "";
+        String json;        
+        String cep;
+        
+		System.out.println("Informe o CEP: ");
+		cep = in.nextLine();
+        try {
+            URL url = new URL("http://viacep.com.br/ws/"+ cep +"/json");
+            URLConnection urlConnection = url.openConnection();
+            InputStream is = urlConnection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            StringBuilder jsonSb = new StringBuilder();
+
+            br.lines().forEach(l -> jsonSb.append(l.trim()));
+            json = jsonSb.toString();
+            
+            // JOptionPane.showMessageDialog(null, json);
+            
+            json = json.replaceAll("[{},:]", "");
+            json = json.replaceAll("\"", "\n");                       
+            String array[] = new String[30];
+            array = json.split("\n");
+            
+            // JOptionPane.showMessageDialog(null, array);
+         	System.out.println("Qual é o número da residência?");
+         	//validarInteiro(numero_residencia);
+         	numero_residencia = in.nextLine();
+         	System.out.println("Qual é o tido do logradouro?");
+         	tipo_logradouro = in.nextLine();
+         	System.out.println("Algum complemento?");
+         	complemento = in.nextLine();
+            
+            logradouro = array[7];            
+            bairro = array[15];
+            cidade = array[19]; 
+            uf = array[23];
+            
+            //criar endereco
+            /*Endereco novoEndereco = new Endereco(cep, tipo_logradouro, logradouro, 
+            							numero_residencia, bairro, cidade, uf, complemento);
+            int retorno = com.serratec.main.Main.cadastrarEndereco().incluirEndereco(novoEndereco);
+            com.serratec.classes.Empresa.adicionarEndereco(novoEndereco);*/
+            endereco ="(" + tipo_logradouro + ") " + logradouro + ", " + numero_residencia + 
+            		" - " + bairro + " - " + cidade + " - " + uf + " - Complemento: " + complemento;
+            System.out.println(Arrays.toString(array));
+            //JOptionPane.showMessageDialog(null, logradouro + " " + bairro + " " + cidade + " " + uf);
+			//System.out.println(logradouro + " " + bairro + " " + cidade + " " + uf);
+            System.out.println(endereco);
+            return endereco;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } 
+    }
+}
